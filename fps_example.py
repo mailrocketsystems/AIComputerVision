be7068d41cb039860f80a5ea6f64958dc4b20ba7
Daniya Niazi <farihadania@hotmail.com>
2021-04-14 01:44:17 +0500

    Remove NoneType error

--- fps_example.py
+++ fps_example.py
@@ -12,23 +12,29 @@ def main():
 
     while True:
         ret, frame = cap.read()
-        frame = imutils.resize(frame, width=800)
-        total_frames = total_frames + 1
+        if(ret):
+            frame = imutils.resize(frame, width=800)
+            total_frames = total_frames + 1
 
-        fps_end_time = datetime.datetime.now()
-        time_diff = fps_end_time - fps_start_time
-        if time_diff.seconds == 0:
-            fps = 0.0
+            fps_end_time = datetime.datetime.now()
+            time_difference = fps_end_time - fps_start_time
+
+            if time_difference.seconds ==0:
+                fps=0.0
+
+            else:
+                fps = total_frames / time_difference.seconds
+
+            fps_text = "FPS : {:.2f}".format(fps)
+
+            cv2.putText(frame , fps_text,(5,30),cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255),1)
+
+            cv2.imshow("Application",frame)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
         else:
-            fps = (total_frames / time_diff.seconds)
-
-        fps_text = "FPS: {:.2f}".format(fps)
-
-        cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
-
-        cv2.imshow("Application", frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
+            cv2.destroyAllWindows()
             break
 
     cv2.destroyAllWindows()
